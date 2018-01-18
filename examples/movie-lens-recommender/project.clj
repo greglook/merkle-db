dd8785ed2156c8a95aa75dc8cfcc090688ed8839
Greg Look <greg@greg-look.net>
2018-01-17 16:11:46 -0800

    Set up profiling agent config.

--- examples/movie-lens-recommender/project.clj
+++ examples/movie-lens-recommender/project.clj
@@ -8,16 +8,17 @@
   [[org.clojure/clojure "1.9.0"]
    [org.clojure/data.csv "0.1.4"]
    [org.clojure/tools.cli "0.3.5"]
-   [gorillalabs/sparkling "2.1.2"]
    [mvxcvi/merkle-db "0.1.0-SNAPSHOT"]
-   [ch.qos.logback/logback-classic "1.1.7"]
-   [org.slf4j/log4j-over-slf4j "1.7.25"] ]
+   [gorillalabs/sparkling "2.1.2"
+    :exclusions [org.objenesis/objenesis]]
+   [org.slf4j/log4j-over-slf4j "1.7.25"]
+   [ch.qos.logback/logback-classic "1.2.3"]]
 
   :main movie-lens.main
 
   :aot
   [clojure.tools.logging.impl
-   merkle-db.spark.table-partitioner
+   merkle-db.spark.key-partitioner
    merkle-db.spark.table-rdd
    movie-lens.main
    sparkling.destructuring
@@ -31,10 +32,20 @@
   :profiles
   {:provided
    {:dependencies
-    [[org.apache.spark/spark-core_2.11 "2.1.1"
+    [[org.apache.spark/spark-core_2.11 "2.2.1"
+      :exclusions [commons-codec
+                   commons-net
+                   log4j
+                   org.apache.commons/commons-compress
+                   org.scala-lang/scala-reflect
+                   org.slf4j/slf4j-log4j12]]
+     [org.apache.spark/spark-mllib_2.11 "2.2.1"
       :exclusions [log4j org.slf4j/slf4j-log4j12]]
-     [org.apache.spark/spark-mllib_2.11 "2.1.1"
-      :exclusions [log4j org.slf4j/slf4j-log4j12]]]}
+     [com.thoughtworks.paranamer/paranamer "2.6"]]}
+
+   :profiler-agent
+   {:jvm-opts
+    ["-javaagent:riemann-jvm-profiler.jar=prefix=movie-lens,host=localhost,localhost-pid?=true"]}
 
    :uberjar
    {:aot :all}})
